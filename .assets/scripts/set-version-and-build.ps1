dbffd96e1cce03473cd7d6cdea18e1c19b2fe3dd
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-02-16 22:33:26 -0500

    refactor: final before release

---
+++ .assets/scripts/set-version-and-build.ps1
@@ -0,0 +1,17 @@
+param (
+    [string]$BuildType = "Release"
+)
+
+# Import shared functions
+Set-Location (Split-Path $MyInvocation.MyCommand.Path)
+. .\utilities.ps1
+
+# Locate solution directory and switch to it
+$solutionDir = Get-SolutionDirectory
+Set-Location $solutionDir
+
+# Ensure GitVersion environment variables are set
+Ensure-GitVersion-Environment 
+
+# Build the project with the version information applied
+Build-Project -Configuration $BuildType
