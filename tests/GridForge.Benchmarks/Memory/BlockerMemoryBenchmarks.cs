842bdf0a169d24a76fc66bc9110987044a3e6d4e
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2026-03-10 23:36:19 -0400

    update: added benchmark testing

---
+++ tests/GridForge.Benchmarks/Memory/BlockerMemoryBenchmarks.cs
@@ -0,0 +1,123 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using FixedMathSharp;
+using GridForge.Blockers;
+using GridForge.Configuration;
+using GridForge.Grids;
+using System;
+
+namespace GridForge.Benchmarks;
+
+[MemoryDiagnoser]
+[ShortRunJob(RuntimeMoniker.Net80)]
+public class BlockerMemoryBenchmarks
+{
+    private BoundingArea[] _areas;
+    private BoundsBlocker[] _blockers;
+
+    public int BlockerCount { get; set; } = 64;
+
+    public int BlockSpan { get; set; } = 6;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _areas = BuildAreas();
+    }
+
+    [IterationSetup(Target = nameof(ApplyAndRemoveBlockers_NoCoverageCache))]
+    public void SetupUncachedIteration()
+    {
+        InitializeScenario(cacheCoveredVoxels: false);
+    }
+
+    [IterationSetup(Target = nameof(ApplyAndRemoveBlockers_CoverageCacheEnabled))]
+    public void SetupCachedIteration()
+    {
+        InitializeScenario(cacheCoveredVoxels: true);
+    }
+
+    [IterationCleanup]
+    public void CleanupIteration()
+    {
+        if (_blockers != null)
+        {
+            foreach (BoundsBlocker blocker in _blockers)
+                blocker.Reset();
+        }
+
+        BenchmarkEnvironment.ResetWorld();
+    }
+
+    [Benchmark(Baseline = true, Description = "Blocker apply/remove without cached coverage")]
+    [BenchmarkCategory("Memory", "Caching", "Blocker")]
+    public int ApplyAndRemoveBlockers_NoCoverageCache()
+    {
+        return ExecuteBlockerWave();
+    }
+
+    [Benchmark(Description = "Blocker apply/remove with cached coverage")]
+    [BenchmarkCategory("Memory", "Caching", "Blocker")]
+    public int ApplyAndRemoveBlockers_CoverageCacheEnabled()
+    {
+        return ExecuteBlockerWave();
+    }
+
+    private void InitializeScenario(bool cacheCoveredVoxels)
+    {
+        BenchmarkEnvironment.PrepareWorld();
+
+        GridConfiguration configuration = new GridConfiguration(
+            new Vector3d(0, 0, 0),
+            new Vector3d(191, 0, 191),
+            scanCellSize: 8);
+
+        if (!GlobalGridManager.TryAddGrid(configuration, out _))
+            throw new InvalidOperationException("Unable to allocate blocker benchmark grid.");
+
+        _blockers = new BoundsBlocker[_areas.Length];
+        for (int i = 0; i < _areas.Length; i++)
+            _blockers[i] = new BoundsBlocker(_areas[i], cacheCoveredVoxels: cacheCoveredVoxels);
+    }
+
+    private BoundingArea[] BuildAreas()
+    {
+        BoundingArea[] areas = new BoundingArea[BlockerCount];
+
+        const int columns = 8;
+        const int stride = 20;
+        for (int i = 0; i < areas.Length; i++)
+        {
+            int row = i / columns;
+            int column = i % columns;
+            int x = 4 + column * stride + (row & 1);
+            int z = 4 + row * stride + (column & 1);
+
+            Vector3d min = new Vector3d(x, 0, z);
+            Vector3d max = new Vector3d(x + BlockSpan, 0, z + BlockSpan);
+            areas[i] = new BoundingArea(min, max);
+        }
+
+        return areas;
+    }
+
+    private int ExecuteBlockerWave()
+    {
+        int appliedCount = 0;
+
+        for (int wave = 0; wave < 8; wave++)
+        {
+            for (int i = 0; i < _blockers.Length; i++)
+            {
+                _blockers[i].ApplyBlockage();
+                if (_blockers[i].IsBlocking)
+                    appliedCount++;
+            }
+
+            for (int i = 0; i < _blockers.Length; i++)
+                _blockers[i].RemoveBlockage();
+        }
+
+        return appliedCount;
+    }
+}
---
+++ tests/GridForge.Benchmarks/Memory/ScanRadiusMemoryBenchmarks.cs
@@ -0,0 +1,112 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using FixedMathSharp;
+using GridForge.Configuration;
+using GridForge.Grids;
+using GridForge.Spatial;
+using System;
+
+namespace GridForge.Benchmarks;
+
+[MemoryDiagnoser]
+[ShortRunJob(RuntimeMoniker.Net80)]
+public class ScanRadiusMemoryBenchmarks
+{
+    private Vector3d _queryCenter;
+    private Fixed64 _queryRadius;
+
+    public int OccupantCount { get; set; } = 16384;
+
+    [IterationSetup(Target = nameof(ScanRadius_ColdPools))]
+    public void SetupColdIteration()
+    {
+        InitializeScenario(clearAllPools: true);
+    }
+
+    [IterationSetup(Target = nameof(ScanRadius_WarmPools))]
+    public void SetupWarmIteration()
+    {
+        InitializeScenario(clearAllPools: false);
+    }
+
+    [IterationCleanup]
+    public void CleanupIteration()
+    {
+        BenchmarkEnvironment.ResetWorld();
+    }
+
+    [Benchmark(Baseline = true, Description = "ScanRadius with cold temporary pools")]
+    [BenchmarkCategory("Memory", "Pooling", "ScanQuery")]
+    public int ScanRadius_ColdPools()
+    {
+        return ExecuteScan();
+    }
+
+    [Benchmark(Description = "ScanRadius with warm temporary pools")]
+    [BenchmarkCategory("Memory", "Pooling", "ScanQuery")]
+    public int ScanRadius_WarmPools()
+    {
+        return ExecuteScan();
+    }
+
+    private void InitializeScenario(bool clearAllPools)
+    {
+        BenchmarkEnvironment.PrepareWorld(clearAllPools);
+
+        int gridSize = 63;
+        for (int gridX = 0; gridX < 2; gridX++)
+        {
+            for (int gridZ = 0; gridZ < 2; gridZ++)
+            {
+                int minX = gridX * (gridSize + 1);
+                int minZ = gridZ * (gridSize + 1);
+                GridConfiguration configuration = new GridConfiguration(
+                    new Vector3d(minX, 0, minZ),
+                    new Vector3d(minX + gridSize, 0, minZ + gridSize),
+                    scanCellSize: 8);
+
+                if (!GlobalGridManager.TryAddGrid(configuration, out _))
+                    throw new InvalidOperationException($"Unable to allocate scan benchmark grid ({gridX}, {gridZ}).");
+            }
+        }
+
+        PopulateOccupants(OccupantCount);
+
+        _queryCenter = new Vector3d(64, 0, 64);
+        _queryRadius = (Fixed64)72;
+    }
+
+    private static void PopulateOccupants(int occupantCount)
+    {
+        int placed = 0;
+        int groupId = 0;
+
+        for (int z = 0; z <= 127 && placed < occupantCount; z++)
+        {
+            for (int x = 0; x <= 127 && placed < occupantCount; x++)
+            {
+                BenchmarkOccupant occupant = new BenchmarkOccupant(
+                    new Vector3d(x, 0, z),
+                    (byte)(groupId++ & 7));
+
+                if (!GridOccupantManager.TryRegister(occupant))
+                    throw new InvalidOperationException($"Unable to register benchmark occupant at {(x, z)}.");
+
+                placed++;
+            }
+        }
+
+        if (placed != occupantCount)
+            throw new InvalidOperationException($"Only placed {placed} of {occupantCount} benchmark occupants.");
+    }
+
+    private int ExecuteScan()
+    {
+        int occupantHits = 0;
+
+        foreach (IVoxelOccupant occupant in GridScanManager.ScanRadius(_queryCenter, _queryRadius))
+            occupantHits++;
+
+        return occupantHits;
+    }
+}
